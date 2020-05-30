d8b8760d9ae74e011e0319e7178083edff843b37
marie-hotchkiss <mtknowlton67@gmail.com>
2020-05-29 20:02:58 -0700

    Done MVP

---
+++ smurfs/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <a href='https://www.stockio.com/free-icon/nature-icons-mushroom'>Icon Source</a>
+            <p>Copyright 2020 Marie Hotchkiss</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { deleteSmurf, editSmurf } from '../actions';
+
+
+
+
+const Smurf = props => {
+    const [editing, setEditing ] = useState(false);
+    const [smurfToEdit, setSmurfToEdit] = useState({name: props.smurf.name, age: props.smurf.age, height: props.smurf.height});
+
+    const handleClick = e => {
+        console.log('Time to delete the smurf of id: ', props.smurf.id);
+        props.deleteSmurf(props.smurf.id);
+    }
+
+    const startEdit = e => {
+        console.log('Time to edit the smurf of id: ', props.smurf.id);
+        setEditing(!editing);
+    }
+
+    const submitForm = e => {
+        e.preventDefault();
+        console.log('Time to submit the form');
+        console.log(smurfToEdit);
+        const smurfToSend = {...smurfToEdit, id: props.smurf.id};
+        console.log(smurfToSend);
+        props.editSmurf(smurfToSend);
+        setEditing(false);
+    }
+
+    const handleChanges = e => {
+        setSmurfToEdit({...smurfToEdit, [e.target.name]: e.target.value});
+    }
+
+    return (
+        <div className='smurf-box'>
+            <button onClick={handleClick}>X</button>
+            <button onClick={startEdit}>{editing? 'Cancel Edit': 'Edit'}</button>
+            <h3>{props.smurf.name}</h3>
+            <div className='imgContainer'>
+               
+            </div>
+            <p>{props.smurf.age} Years Old</p>
+            <p>{props.smurf.height} High</p>
+
+            {editing && <form onSubmit={submitForm}>
+            <label htmlFor='name'>Name: </label>
+            <input
+                id='name'
+                name='name'
+                type='text'
+                onChange={handleChanges}
+                value={smurfToEdit.name}
+            />
+            <br />
+
+            <label htmlFor='age'>Age: </label>
+            <input
+                id='age'
+                name='age'
+                type='number'
+                onChange={handleChanges}
+                value={smurfToEdit.age}
+            />
+            <br />
+
+            <label htmlFor='height'>Height: </label>
+            <input
+                id='height'
+                name='height'
+                type='text'
+                onChange={handleChanges}
+                value={smurfToEdit.height}
+            />
+            <br />
+
+            <button type='submit'>Submit Smurf Changes</button>
+
+        </form>}
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return state;
+}
+
+export default connect(mapStateToProps, { deleteSmurf, editSmurf })(Smurf);
